ac94b1fc68b5f4d4445ea76235d6f549b68bbd80
jalanis00 <FJam07@gmail.com>
2018-09-19 23:13:01 -0500

    Added edit functionality.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,44 @@
 class PortfoliosController < ApplicationController
+    
+    def index
+        @portfolio_items = Portfolio.all
+    end
+
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+        respond_to do |format|
+                if @portfolio_item.save
+                    format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live!' }
+                    format.json { render :show, status: :created, location: @portfolio }
+                else
+                    format.html { render :new }
+                    format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+                end
+        end
+    end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+
+        respond_to do |format|
+            if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+                format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+                format.json { render :show, status: :ok, location: @portfolio }
+            else
+                format.html { render :edit }
+                format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+  
+
 end
