14103ba6425cc8421da9b3bfdd4b7d0cbd8e253a
jalanis00 <FJam07@gmail.com>
2018-09-29 18:46:56 -0500

    Added custom toggle for updating status.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact',  to: 'pages#contact'
-  resources :blogs
+  
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   root to: 'pages#home'
-
 end
